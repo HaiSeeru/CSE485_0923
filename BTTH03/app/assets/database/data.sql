704dbda137ef777fc9988c4ff8fa31277b1f5f86
twor7915 <tranhai7915@gmail.com>
2023-10-06 10:47:55 +0700

    add file sql

---
+++ BTTH03/app/assets/database/data.sql
@@ -0,0 +1,24 @@
+create database QuanLyBaiHat
+use QuanLyBaiHat
+
+CREATE TABLE TheLoai(
+	id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    tenTheLoai VARCHAR(255) NOT NULL
+)
+
+
+CREATE TABLE BaiHat(
+	id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    tenBaiHat VARCHAR(255) NOT NULL,
+    caSi varchar(255) not null,
+    idTheLoai INT unsigned not null,
+	foreign key(idTheLoai) references TheLoai(id)
+)
+drop table BaiHat
+insert into TheLoai(tenTheLoai) values
+	('Nhạc trẻ'),
+	('Nhạc vàng');
+    
+insert into BaiHat(tenBaiHat, caSi, idTheLoai) values
+	('Anh yêu em', 'Sơn Tùng', 1),
+	('1 phút', 'Quang Lâm', 2 );
